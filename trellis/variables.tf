44c79fa14b10c5e5f9703c296a79711881728c7e
Paul Billing-Ross <pbilling@stanford.edu>
2020-04-10 13:32:34 -0700

    Add default values

--- trellis/variables.tf
+++ trellis/variables.tf
@@ -1,7 +1,13 @@
+// Variables requiring user-specified values
 variable "project" {
     type = string
 }
 
+variable "external-bastion-ip" {
+    type = string
+}
+
+// Variables with default values
 variable "region" {
     type = string
     default = "us-west1"
@@ -16,14 +22,6 @@ variable "data-group" {
     type = string
     default = "phase3"
 }
-
-variable "external-bastion-ip" {
-    type = string
-}
-
-//variable "stable-cos-image" {
-//    type = string
-//}
 
 variable "neo4j-image" {
     default = "neo4j:3.5.4"
@@ -42,25 +40,32 @@ variable "neo4j-heap-size" {
 variable "github-owner" {
     type = string
     description = "Owner of the Trellis GitHub repo."
+    default = "StanfordBioinformatics"
 }
 
 variable "github-repo" {
     type = string
     description = "Name of the Trellis GitHub repo."
+    default = "trellis-mvp-functions"
 }
 
 variable "github-branch-pattern" {
     type = string
+    default = "master"
 }
 
 variable "gatk-github-owner" {
-    type = string
+    type        = string
+    description = "Owner of the repo with GATK pipeline config objects."
+    default     = "StanfordBioinformatics"
 }
 
 variable "gatk-github-repo" {
     type = string
+    default = "gatk-mvp"
 }
 
 variable "gatk-github-branch-pattern" {
     type = string
+    default = "^no-address$"
 }
