a2a7668b1da050baa4cd4978bbefd6da96b5e0d3
Paul Billing-Ross <pbilling@stanford.edu>
2020-04-16 14:15:51 -0700

    Remove compute metadata properties

--- trellis/main.tf
+++ trellis/main.tf
@@ -18,10 +18,3 @@ provider "google-beta" {
   zone    = var.zone
 }
 
-
-resource "google_compute_project_metadata" "default" {
-    metadata = {
-        trellis-network = google_compute_network.trellis-vpc-network.name
-        trellis-subnetwork = google_compute_subnetwork.trellis-subnet.name
-    }
-}
