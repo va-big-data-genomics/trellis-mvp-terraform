35231859c1830cc86fbed22f0d01c351825bd225
Paul Billing-Ross <pbilling@stanford.edu>
2020-05-20 14:55:05 -0700

    Added cloud functions service

--- trellis/main.tf
+++ trellis/main.tf
@@ -18,3 +18,11 @@ provider "google-beta" {
   zone    = var.zone
 }
 
+/* Not sure what this is for
+resource "google_project_service" "project" {
+  project = var.project
+  service = "cloudfunctions.googleapis.com"
+
+  disable_dependent_services = true
+}
+*/
